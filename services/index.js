412b3bc58c6986dd88b6119a22fc226b49a54cf4
Danish <khand8914@gmail.com>
2023-09-17 00:11:46 +0530

    added cors

--- services/index.js
+++ services/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const http = require('http');
-const socketIO = require('socket.io');
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+// const io = socketIO(server);
 const port = 3000;
+const cors = require('cors');
+corsOptions = {
+    origin: "*",
+}
+app.use(cors(corsOptions));
+const io = require('socket.io')(http,{
+    cors: {
+        origin: "*",
+}
+});
 
 io.on('connection', socket => {
     console.log('socket connestion established');
